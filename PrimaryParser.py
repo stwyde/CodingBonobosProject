941e3aaaaabefc2c63d2a91861c9963bf434b466
talhaahsan <stwyde@gmail.com>
2017-04-21 16:33:20 -0500

    todo: PURGE THE COMMON WORDS

--- PrimaryParser.py
+++ PrimaryParser.py
@@ -7,15 +7,23 @@ class ParsedEntry:
     title = ""
     text = ""
     url = ""
+    occuranceTable = {}
     def __init__(self, name, body, link):
         self.url = link
         lowercaseText = body.lower()
         newStr = ""
         for c in lowercaseText:
-            if c.isalpha():
+            if c.isalpha() or c==" ":
                 newStr+=c
         self.text = newStr
         self.title = name
+        wordsList = self.text.split()
+        for word in wordsList:
+            if word not in self.occuranceTable.keys():
+                self.occuranceTable[word] = 0
+            self.occuranceTable[word] = self.occuranceTable[word] + 1
+
+
 
 for entry in NYTAmericas.entries:
     print(entry.link)
@@ -23,4 +31,4 @@ for entry in NYTAmericas.entries:
     toParse.download()
     toParse.parse()
     parsedArticle = ParsedEntry(toParse.title, toParse.text, entry.link)
-    print((parsedArticle.text))
+    print((parsedArticle.occuranceTable))
