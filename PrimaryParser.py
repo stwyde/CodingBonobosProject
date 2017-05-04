b95190fc202b5f4489621f4e725c77fb882a410b
M Ulmer <mulmer@macalester.edu>
2017-05-04 07:39:40 -0500

    added comments, little bit of clean-up

--- PrimaryParser.py
+++ PrimaryParser.py
@@ -5,7 +5,6 @@ from collections import Counter
 import numpy
 from sklearn.cluster import KMeans
 from sklearn import metrics
-#import matplotlib.pyplot as plt
 
 NYTAmericas = feedparser.parse("http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml")
 NYTEurope = feedparser.parse("http://rss.nytimes.com/services/xml/rss/nyt/Europe.xml")
@@ -16,16 +15,21 @@ NYTPol = feedparser.parse("http://rss.nytimes.com/services/xml/rss/nyt/Politics.
 removeSet = set(stopwords.words('english'))
 removeSet.update(['continue', 'reading', 'lately', 'wanted', 'call', 'later', 'latest', 'main', 'said','way', 'many', 'available', 'efforts', 'similar', 'programadvertisement', 'multiple', 'months' 'essentially', 'identify', 'include', 'name'])
 class ParsedEntry:
-    #Not called article to avoid conflict with Newspaper package
+    """Object which contains information about each article, including its name and
+    cleaned text. Includes function for preprocessing.
+    Not called article to avoid conflict with Newspaper package"""
     def __init__(self, name, body, link):
-        
         self.url = link
         self.title = name
         self.tagTable, self.cleanWordsList = self._preprocessText(body)
 
     def _preprocessText(self, body):
+        """casefolds text and removes stopwords, returns a list of the article's 
+        words and a dictionary where the keys are unique words and the values are
+        their occurrence counts.
+        cleanWordsList is not currently used, but could be used to normalize the
+        number of word occurences, so we kept it for future flexibility."""
         tagTable = {}
-        #sanitizes text, sets everything to lowercase, removes numbers and symbols but keeps spaces leading to just words separated by spaces.
         lowercaseText = body.lower()
         newStr = ""
         for c in lowercaseText:
@@ -33,7 +37,7 @@ class ParsedEntry:
                 newStr+=c
         self.text = newStr
 
-        #creates dictionary with word occurances in the occurrenceTable dictionary object.
+        #remove stopwords and build dictionary of wordcounts.
         wordsList = self.text.split()
         cleanWordsList = []
         for word in wordsList:
@@ -46,6 +50,7 @@ class ParsedEntry:
         return tagTable, cleanWordsList
 
 
+    #given an integer n, returns the n most frequently appearing keywords in the article
     def getTopTags(self, number):
         #http://stackoverflow.com/questions/11902665/top-values-from-dictionary
         top = Counter(self.tagTable)
@@ -54,29 +59,11 @@ class ParsedEntry:
         for k, v in top.most_common(10):
             topTags[k] = v
         return topTags
-
-def articleDistance(art1, art2):
-    """returns the pairwise distance between two artilces, calculated from the tags"""
-    tags1 = list(art1.tagTable.keys())
-    tags2 = list(art2.tagTable.keys())
-    tags = tags1[:]
-    tags.append(tags2)
-
-    distance = 0
-
-    for tag in tags:
-        value1 = 0
-        value2 = 0
-        if tag in tags1:
-            value1 = art1.tagTable[tag]
-        if tag in tags2:
-            value2 = art2.tagTable[tag]
-        distance = distance + abs(value1 - value2)
-        distance = distance / len(tags)
-
-    return distance
     
 def initTags(topTags, tagSet, article):
+        """Given an article and it's n most commonly occurring words, adds each
+    article to the list of articles associated with that tag. If the tag is
+    not already in the set of tags, create it."""
     for tag in topTags:
         if tag in tagSet.keys():
             tagSet[tag].append(article)
@@ -84,15 +71,19 @@ def initTags(topTags, tagSet, article):
             tagSet[tag] = [article]
 
 def tagDistanceMatrix(tagSet, labels, articleSet):
-    """builds binary array filled with which articles have what tags"""
-    #for article in articleSet:
-    #    for tag in tagSet:
-    #        print(article in tagSet[tag])
+    """Builds a binary array denoting which articles have which tags. Rows represent
+    articles and columns represent tags, so 1 in position i, j means that the article
+    i is tagged with keyword j. 'labels' is just the set of keys of tagSet (that is,
+    the list of all keys) - it's passed as a list to ensure the ordering of the tags
+    is the same as it will be later.
+    Also: look at that list comprehension! I'm super proud of myself."""
     distances = numpy.array([[(article in tagSet[tag]) for article in articleSet] 
           for tag in labels])
     return distances        
     
 def feedArticleParsing(feed):
+    """Procedure for reading in articles from a given feed.
+    For each article in the RSS feed, grab it, process it, and find its tags"""
     global i
     for entry in feed.entries:
         toParse = Article(entry.link)
@@ -117,18 +108,25 @@ feedArticleParsing(NYTWorld)
 
 
 
-tagFlag = False
-print("Clustering tags, not articles: ", tagFlag)
-print("Articles: " + i.__str__())
-labels = tagSet.keys() #want to ENSURE the ordering is the same now and later
-print("Labels: " + len(labels).__str__())
-print("Beginning K means now with k=1:")
+tagFlag = False #True if we want to cluster tags, false if we want to cluster articles
+labels = list(tagSet.keys()) #want to ENSURE the ordering is the same now and later
+
+#calculate the binary matrix representing which articles have which tags
 distances = tagDistanceMatrix(tagSet, labels, articleSet)
 if tagFlag is False:
     distances = numpy.transpose(distances)
+    labels = [article.title for article in articleSet] #make labels article names, not tags
+    
+    
+print("Clustering tags, not articles: ", tagFlag)
+print("Articles: " + i.__str__())
+print("Labels: " + len(labels).__str__())
+print("Beginning K means now with k=1:")
+
 km = KMeans(n_clusters = 1, random_state = 0).fit(distances)
 initInertia = km.inertia_
 print(initInertia)
+#attempts to find the optimal k by stopping clustering when efficnecy lowers
 elbowed = False
 k = 1
 while not elbowed and k < len(articleSet)-1:
@@ -142,12 +140,10 @@ while not elbowed and k < len(articleSet)-1:
     else:
         initInertia = newInertia
 
-
+#print out the clusters - each number represents the cluster number of the object at that index
 print(km.labels_)
-labels = list(labels)
-if tagFlag is False: #make labels article names, not tags
-    labels = [article.title for article in articleSet]
-clusters = [[] for i in range (0, k)]
+clusters = [[] for i in range (0, k)] #each entry in this list is a list of the items in the cluster
+#fill up the cluster lists with the items
 for i in range(0, len(km.labels_)):
     clusters[km.labels_.item(i)].append(labels[i])
 for i in range (0, k):
